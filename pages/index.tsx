1ed6c2383342fb4123184def88d942c94e123aa4
Harald Solaas <haraldsolaas@Haralds-MacBook-Pro.local>
2021-03-11 17:20:43 -0300

    Initial setup * Clear unused boilerplate * Add dependencies: ts and styled-components.

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hari Solaas</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to my site!</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
